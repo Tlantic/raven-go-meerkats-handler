2a38c1831a3edae52b8947317044f746ea9fd147
João Pinho <joaolpinho@gmail.com>
2016-12-21 17:46:40 +0000

    Initial Commit

--- raven.go
+++ raven.go
@@ -2,78 +2,170 @@ package raven_meerkats
 
 import (
 	"fmt"
-	Meerkats "github.com/Tlantic/meerkats"
-	Raven "github.com/getsentry/raven-go"
+	log "github.com/Tlantic/meerkats"
+	"github.com/getsentry/raven-go"
+	"sync"
+	"time"
 )
 
 const (
 	FIELD_TAGS = "Tags"
 )
 
-var levels = [...]Raven.Severity {
-	Meerkats.LEVEL_TRACE: Raven.DEBUG,
-	Meerkats.LEVEL_DEBUG: Raven.DEBUG,
-	Meerkats.LEVEL_INFO: Raven.INFO,
-	Meerkats.LEVEL_WARNING: Raven.WARNING,
-	Meerkats.LEVEL_ERROR: Raven.ERROR,
-	Meerkats.LEVEL_FATAL: Raven.FATAL,
-	Meerkats.LEVEL_PANIC: Raven.FATAL,
+var levels = [...]raven.Severity {
+	log.TRACE: raven.DEBUG,
+	log.DEBUG: raven.DEBUG,
+	log.INFO: raven.INFO,
+	log.WARNING: raven.WARNING,
+	log.ERROR: raven.ERROR,
+	log.FATAL: raven.FATAL,
+	log.PANIC: raven.FATAL,
 }
 
-
+var _ log.Handler = (*RavenHandler)(nil)
 type RavenHandler struct {
-	*Raven.Client
+	*raven.Client
+	Level log.Level
+	TimeLayout string
+	mu sync.Mutex
+	tags map[string]string
+	fields map[string]interface{}
 }
+
+
 
 
 //noinspection GoUnusedExportedFunction
-func New(dsn string, tags map[string]string) (*RavenHandler, error) {
-
-	if ( tags == nil ) {
-		tags = make(map[string]string)
+func New(options...log.HandlerOption) (h *RavenHandler) {
+	h = &RavenHandler{
+		Client: raven.DefaultClient,
+		Level: log.LEVEL_ALL,
+		TimeLayout: "",
+		mu: sync.Mutex{},
+		tags: map[string]string{},
+		fields: map[string]interface{}{},
 	}
-
-	tags["logger"] = "meerkats"
-
-	if client, err := Raven.NewClient(dsn, tags); err != nil {
-		return nil, err
-	} else {
-		return &RavenHandler{client}, nil
+	for _, opt := range options {
+		opt.Apply(h)
 	}
+	return h
+}
 
+func Register(options ...log.HandlerOption) log.LoggerOption {
+	return log.LoggerReceiver(func(l log.Logger) {
+		l.Register(New(options...))
+	})
 }
 
 
-func (h *RavenHandler) HandleEntry(e Meerkats.Entry, done Meerkats.Callback) {
-	defer done()
+func (h *RavenHandler) AddBool(key string, value bool) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.fields[key] = value
+}
+func (h *RavenHandler) AddString(key string, value string) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.fields[key] = value
+}
+func (h *RavenHandler) AddInt(key string, value int) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.fields[key] = value
+}
+func (h *RavenHandler) AddInt64(key string, value int64) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.fields[key] = value
+}
+func (h *RavenHandler) AddUint(key string, value uint) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.fields[key] = value
+}
+func (h *RavenHandler) AddUint64(key string, value uint64) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.fields[key] = value
+}
+func (h *RavenHandler) AddFloat32(key string, value float32) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.fields[key] = value
+}
+func (h *RavenHandler) AddFloat64(key string, value float64) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.fields[key] = value
+}
+func (h *RavenHandler) Add(key string, value interface{}) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.fields[key] = value
+}
+func (h *RavenHandler) With(fields ...log.Field) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	for _, f := range fields {
+		h.fields[f.Key] = f.Get()
+	}
+}
 
-	p := Raven.NewPacket(e.Message)
-	p.EventID = e.TraceId
-	p.Timestamp = Raven.Timestamp(e.Timestamp)
-	p.Level = levels[e.Level]
-
-
-
-	tags := make(map[string]string)
-	t := Meerkats.Fields{}
-	if  e.Fields[FIELD_TAGS] != nil {
-		t.Merge(e.Fields[FIELD_TAGS])
-		for key, value := range t {
-			tags[key] = fmt.Sprint(value)
+func (h *RavenHandler) Log(t time.Time, level log.Level, msg string, fields []log.Field, meta map[string]string) {
+	if ( h.Level&level != 0 ) {
+		packet := raven.NewPacket(msg)
+		packet.Level = levels[level]
+		packet.AddTags(h.Tags)
+		packet.AddTags(meta)
+		packet.Logger = "meerkats"
+		packet.Timestamp = raven.Timestamp(t)
+		for k, v := range h.fields {
+			packet.Extra[k] = v
 		}
-		delete(e.Fields, FIELD_TAGS)
+		for _, f := range fields {
+			packet.Extra[f.Key] = f.Get()
+		}
+
+		id, ch := h.Capture(packet, h.tags);
+		err := <- ch
+		if err != nil {
+			fmt.Errorf("%s: %s", id, err.Error())
+		}
 	}
+}
+
+func (h *RavenHandler) SetTimeLayout(layout string) {
+	h.TimeLayout = layout
+}
+func (h *RavenHandler) GetTimeLayout() string {
+	return h.TimeLayout
+}
+
+func (h *RavenHandler) SetLevel(level log.Level) {
+	h.Level = level
+}
+func (h *RavenHandler) GetLevel() log.Level {
+	return h.Level
+}
 
 
-	for key, value := range e.Fields {
-		p.Extra[key] = value
+func (h *RavenHandler) Clone() log.Handler {
+	clone := &RavenHandler{
+		Client: h.Client,
+		Level: h.Level,
+		TimeLayout: h.TimeLayout,
+		tags: map[string]string{},
+		fields: map[string]interface{}{},
+		mu: sync.Mutex{},
 	}
-
-
-
-	id, ch := h.Capture(p, tags);
-	err := <- ch
-	if err != nil {
-		fmt.Errorf("%s: %s", id, err.Error())
+	for k, v := range h.tags {
+		clone.tags[k] = v
 	}
+	for k, v := range h.fields {
+		clone.fields[k] = v
+	}
+	return clone
+}
+func (h *RavenHandler) Dispose() {
+
 }
--- raven_test.go
+++ raven_test.go
@@ -3,59 +3,22 @@ package raven_meerkats
 import (
 	"testing"
 	"github.com/Tlantic/meerkats"
+	"github.com/getsentry/raven-go"
 )
 
-const (
-	DSN = "https://5f0a7eb4fbf74558b5f4e01ca650bc72:f8413f5aac2e4370a550a31cf73d6489@sentry.io/115378"
-)
+var err = raven.SetDSN("https://5f0a7eb4fbf74558b5f4e01ca650bc72:f8413f5aac2e4370a550a31cf73d6489@sentry.io/115378")
 
 var TAGS = map[string]string{
 	"test": "true",
 }
 
 func TestNew(t *testing.T) {
-	r , err := New(DSN, TAGS)
-	if err != nil {
-		t.Fatal(r)
+	if (err != nil) {
+		t.Fatalf("%s", err.Error())
 	}
 
-	// assert interface
-	var _ meerkats.EntryHandler = r.HandleEntry
-}
-
-func TestRavenHandler_HandleEntry(t *testing.T) {
-
-	r , err := New(DSN, TAGS)
-	if err != nil {
-		t.Fatal(r)
-	}
-
-	m := meerkats.New(nil)
-	defer func() {
-		if r := recover(); r != nil {
-			m.Panic(r)
-		}
-		m.Close()
-	}()
-	m.RegisterHandler(meerkats.LEVEL_ALL, r.HandleEntry)
-
-	entry := m.WithFields(meerkats.Fields{
-		"Field1": true,
-		"Field2": "true",
-		"Field3": nil,
-		"Field4": false,
-		FIELD_TAGS: meerkats.Fields{
-			"Tag1": "false",
-			"Tag2": "true",
-			"Tag3": "nil",
-			"Tag4": "null",
-		},
-	})
-/*
-	entry.Trace("Trace message")
-	entry.Debug("Debug message")
-	entry.Info("Info message")
-	entry.Warning("Warning message")
-	entry.Error("Error message")*/
-	entry.Fatal("Fatal message")
+	logger := meerkats.New(meerkats.TRACE, Register())
+	logger.SetMeta("isMeta", "true")
+	logger.With(meerkats.String("id", "12345"))
+	logger.Error("hello", meerkats.Bool("test", true))
 }
